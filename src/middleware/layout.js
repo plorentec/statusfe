bdf9bf5362303dfd5335a2541843af1275bbdf89
plorentec <pablo@plorentec.com>
2026-06-04 15:28:58 +0200

    Fix: escape EJS tags in layout partial content to prevent JS code rendering

--- src/middleware/layout.js
+++ src/middleware/layout.js
@@ -2,11 +2,19 @@ const path = require('path');
 const ejs = require('ejs');
 const fs = require('fs');
 
+function escapeEJS(str) {
+  return str
+    .replace(/<%/g, '\u200B<%')
+    .replace(/%>/g, '%>\u200B');
+}
+
 function layout(view, locals) {
   const layoutPath = path.join(__dirname, '..', '..', 'views', 'admin.ejs');
   const viewPath = path.join(__dirname, '..', '..', 'views', 'admin', view + '.ejs');
   
-  const viewContent = fs.readFileSync(viewPath, 'utf8');
+  let viewContent = fs.readFileSync(viewPath, 'utf8');
+  viewContent = escapeEJS(viewContent);
+  
   const layoutContent = fs.readFileSync(layoutPath, 'utf8');
   
   const merged = { ...locals };
